3b21a1fbfe3361309bf0d43022b12d9a74686d23
sash74 <118597640+sash74@users.noreply.github.com>
2022-11-19 16:51:17 +0100

    Update sash74.sh

--- sash74.sh
+++ sash74.sh
@@ -3,10 +3,10 @@ clignoter="$(printf '\033[5m')"
 RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
 MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
 RESETBG="$(printf '\e[0m\n')"
-echo -e "\n ${WHITE}${clignoter}                      +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-${WHITE}${clignoter}                       +${RESETBG}${RED}                         #     ${WHITE}${clignoter}                              +
-${WHITE}${clignoter}                       + ${RESETBG}${CYAN}    mmm     mmm   mmm   # mm    mmm ${WHITE}${clignoter}                        +
-${WHITE}${clignoter}                       + ${RESETBG}${ORANGE}   #   °  °   #  #   °  #°  #  °   # ${WHITE}${clignoter}                       +
-${WHITE}${clignoter}                       + ${RESETBG}${MAGENTA}    °°°m  m°°°#   °°°m  #   #  m°°°# ${WHITE}${clignoter}                       +
-${WHITE}${clignoter}                       + ${RESETBG}${GREEN}   °mmm°  °mm°#  °mmm°  #   #  °mm°# ${WHITE}${clignoter}                       +
-${WHITE}${clignoter}                       +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ \n"
+echo -e "\n ${WHITE}${clignoter}                      ++++++++++++++++++++++++++++++++++++++++++++
+${WHITE}${clignoter}                       +${RESETBG}${RED}                         #     ${WHITE}${clignoter}           +
+${WHITE}${clignoter}                       + ${RESETBG}${CYAN}    mmm     mmm   mmm   # mm    mmm ${WHITE}${clignoter}     +
+${WHITE}${clignoter}                       + ${RESETBG}${ORANGE}   #   °  °   #  #   °  #°  #  °   # ${WHITE}${clignoter}    +
+${WHITE}${clignoter}                       + ${RESETBG}${MAGENTA}    °°°m  m°°°#   °°°m  #   #  m°°°# ${WHITE}${clignoter}    +
+${WHITE}${clignoter}                       + ${RESETBG}${GREEN}   °mmm°  °mm°#  °mmm°  #   #  °mm°# ${WHITE}${clignoter}    +
+${WHITE}${clignoter}                       ++++++++++++++++++++++++++++++++++++++++++++ \n"
